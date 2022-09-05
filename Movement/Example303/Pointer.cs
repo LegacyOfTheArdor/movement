fedfe7b1bf0730cb5c3fe739a83d906e7a4d92b3
LegacyOfTheArdor <95230814+LegacyOfTheArdor@users.noreply.github.com>
2022-09-05 09:09:58 +0200

    pointer and spaceship

--- Movement/Example303/Pointer.cs
+++ Movement/Example303/Pointer.cs
@@ -21,54 +21,61 @@ Methods:
 
 namespace Movement
 {
-	class Pointer : SpriteNode
-	{
-		// your private fields here (add Velocity, Acceleration, and MaxSpeed)
+    class Pointer : SpriteNode
+    {
+        // your private fields here (add Velocity, Acceleration, and MaxSpeed)
+        Vector2 velocity = new Vector2 (5, 5);
+        Vector2 acceleration = new Vector2 (0,0);
 
+        // constructor + call base constructor
+        public Pointer() : base("resources/spaceship.png")
+        {
+            Position = new Vector2(Settings.ScreenSize.X / 2, Settings.ScreenSize.Y / 2);
+            Color = Color.YELLOW;
+        }
 
-		// constructor + call base constructor
-		public Pointer() : base("resources/spaceship.png")
-		{
-			Position = new Vector2(Settings.ScreenSize.X / 2, Settings.ScreenSize.Y / 2);
-			Color = Color.YELLOW;
-		}
+        // Update is called every frame
+        public override void Update(float deltaTime)
+        {
+            PointToMouse(deltaTime);
+            BounceEdges();
+        }
 
-		// Update is called every frame
-		public override void Update(float deltaTime)
-		{
-			PointToMouse(deltaTime);
-			BounceEdges();
-		}
+        // your own private methods
+        private void PointToMouse(float deltaTime)
+        {
+            // Or just implement it in Example 110 Follower
 
-		// your own private methods
-		private void PointToMouse(float deltaTime)
-		{
-			// Or just implement it in Example 110 Follower
+            Vector2 mouse = Raylib.GetMousePosition();
+            // Console.WriteLine(mouse);
 
-			Vector2 mouse = Raylib.GetMousePosition();
-			// Console.WriteLine(mouse);
+            Vector2 v = mouse - Position; 
 
-			Position = mouse; // incorrect!!
+            Vector2 nv = Vector2.Normalize(v);
 
-			Rotation += deltaTime * Math.PI;  // incorrect!!
+            Rotation += deltaTime * Math.PI;  // correct!!
 
-			// TODO implement
-			// Position += Velocity * deltaTime;
-		}
+            // TODO implement
+            // Position += Velocity * deltaTime;
+             Position += velocity * deltaTime;
+             velocity += acceleration * deltaTime;
+             acceleration = nv * 1000;
+             Rotation = Math.Atan2(velocity.Y, velocity.X);
+        }
 
-		private void BounceEdges()
-		{
-			float scr_width = Settings.ScreenSize.X;
-			float scr_height = Settings.ScreenSize.Y;
-			float spr_width = TextureSize.X;
-			float spr_heigth = TextureSize.Y;
+        private void BounceEdges()
+        {
+            float scr_width = Settings.ScreenSize.X;
+            float scr_height = Settings.ScreenSize.Y;
+            float spr_width = TextureSize.X;
+            float spr_heigth = TextureSize.Y;
 
-			// TODO implement...
-			if (Position.X > scr_width)
-			{
-				// ...
-			}
-		}
+            // TODO implement...
+            if (Position.X > scr_width)
+            {
+                // ...
+            }
+        }
 
-	}
+    }
 }
--- Movement/Exercise305/SpaceShip.cs
+++ Movement/Exercise305/SpaceShip.cs
@@ -21,71 +21,84 @@ Methods:
 
 namespace Movement
 {
-	class SpaceShip : SpriteNode
-	{
-		// your private fields here (rotSpeed, thrustForce)
-		private float rotSpeed;
-		// private float thrustForce;
+    class SpaceShip : MoverNode
+    {
+        // your private fields here (rotSpeed, thrustForce)
+        private float rotSpeed;
+        private float thrustForce;
+        
 
-		// constructor + call base constructor
-		public SpaceShip() : base("resources/spaceship.png")
-		{
-			rotSpeed = (float)Math.PI; // rad/second
-			// thrustForce = 500;
+        // constructor + call base constructor
+        public SpaceShip() : base("resources/spaceship.png")
+        {
+            rotSpeed = (float)Math.PI; // rad/second
+            thrustForce = 500;
 
-			Position = new Vector2(Settings.ScreenSize.X / 2, Settings.ScreenSize.Y / 2);
-			Color = Color.YELLOW;
-		}
+            Position = new Vector2(Settings.ScreenSize.X / 2, Settings.ScreenSize.Y / 2);
+            Color = Color.ORANGE;
+        }
 
-		// Update is called every frame
-		public override void Update(float deltaTime)
-		{
-			Move(deltaTime);
-			WrapEdges();
-		}
+        // Update is called every frame
+        public override void Update(float deltaTime)
+        {
+            Move(deltaTime);
+            WrapEdges();
+        }
 
-		// your own private methods
-		private void Move(float deltaTime)
-		{
-			// TODO implement
-			// Position += Velocity * deltaTime;
-		}
+        // your own private methods
+        
 
-		private void WrapEdges()
-		{
-			float scr_width = Settings.ScreenSize.X;
-			float scr_height = Settings.ScreenSize.Y;
-			float spr_width = TextureSize.X;
-			float spr_heigth = TextureSize.Y;
+        private void WrapEdges()
+        {
+            float scr_width = Settings.ScreenSize.X;
+            float scr_height = Settings.ScreenSize.Y;
+            float spr_width = TextureSize.X;
+            float spr_heigth = TextureSize.Y;
 
-			// TODO implement...
-			if (Position.X > scr_width)
-			{
-				// ...
-			}
-		}
+            // TODO implement...
+            if (Position.X > scr_width)
+            {
+                Position.X = 0;
+            } 
+            else if (Position.X < 0) 
+            {
+            Position.X = scr_width;
+            }
 
-		public void RotateRight(float deltaTime)
-		{
-			Rotation += rotSpeed * deltaTime;
-		}
+            if (Position.Y > scr_height)
+            {
+                Position.Y = 0;
+            } 
+            else if (Position.Y < 0) 
+            {
+            Position.Y = scr_height;
+            }
+        }
 
-		public void RotateLeft(float deltaTime)
-		{
-			Rotation -= rotSpeed * deltaTime;
-		}
+        public void RotateRight(float deltaTime)
+        {
+            Rotation += rotSpeed * deltaTime;
+        }
 
-		public void Thrust()
-		{
-			// TODO implement
-			Color = Color.ORANGE;
-			// use thrustForce somewhere here
-		}
+        public void RotateLeft(float deltaTime)
+        {
+            Rotation -= rotSpeed * deltaTime;
+        }
 
-		public void NoThrust()
-		{
-			Color = Color.YELLOW;
-		}
+        public void Thrust()
+        {
+            // TODO implement
+            Color = Color.BLUE;
+            
+            Acceleration.X += thrustForce * (float)Math.Cos(Rotation);
+            Acceleration.Y += thrustForce * (float)Math.Sin(Rotation);
+        }
 
-	}
+        public void NoThrust()
+        {
+            Color = Color.ORANGE;
+            
+        }
+
+    }
 }
